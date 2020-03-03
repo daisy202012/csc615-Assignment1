69ee121796596e062b94580b4a1c8574d26ee342
Daisy <Daisysanchez202012@gmail.com>
2020-03-02 18:45:20 -0800

    Change

--- trafficlights.c
+++ trafficlights.c
@@ -12,25 +12,19 @@
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
 
-  while(1) {
+  for(;;) {
     // Red is on 
     digitalWrite(RED, HIGH);
-    digitalWrite(YELLOW, LOW);
-    digitalWrite(GREEN, LOW);
-
     //pauses program for the amount of time (in miliseconds) specified as parameter 
     delay(5000);
 
     // Green is on 
-    digitalWrite(RED, LOW);
-    digitalWrite(YELLOW, LOW);
     digitalWrite(GREEN, HIGH);
     delay(6000);
 
     // Yellow is on
-    digitalWrite(RED, LOW);
+   
     digitalWrite(YELLOW, High);
-    digitalWrite(GREEN, LOW);
     delay(1500);
 
   }
