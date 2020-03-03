49f2976e57ffb983e199c09995d49862f7431545
Daisy <Daisysanchez202012@gmail.com>
2020-03-02 18:50:01 -0800

    Edit

--- trafficlights.c
+++ trafficlights.c
@@ -7,28 +7,36 @@
 #define YELLOW 4
 #define GREEN 5 
 
+  int main (void){
+  
   wiringPiSetUp();
   pinMode(1, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
 
-  for(;;) {
+  while(1) {
     // Red is on 
     digitalWrite(RED, HIGH);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(GREEN, LOW);
+
     //pauses program for the amount of time (in miliseconds) specified as parameter 
     delay(5000);
 
     // Green is on 
+    digitalWrite(RED, LOW);
+    digitalWrite(YELLOW, LOW);
     digitalWrite(GREEN, HIGH);
     delay(6000);
 
     // Yellow is on
-   
+    digitalWrite(RED, LOW);
     digitalWrite(YELLOW, High);
+    digitalWrite(GREEN, LOW);
     delay(1500);
 
   }
-
+  }
   // Never reached, keeps the compiler happy.
   return 0;
 }
